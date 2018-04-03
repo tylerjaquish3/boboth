e54895eb2f18c9ae67df940345b7aca100dddbf7
tylerjaquish3 <tylerjaquish@gmail.com>
2018-04-02 21:23:06 -0700

    Removed unnecessary assets and added code to send contact email.

--- review_send.php
+++ review_send.php
@@ -133,11 +133,11 @@ Author: Tyler Jaquish, 2015
 <script src="js/jquery-1.8.2.min.js" type="text/javascript"></script> 
 <script src="js/bootstrap.min.js" type="text/javascript"></script> 
 <script src="js/jquery.isotope.min.js" type="text/javascript"></script> 
-<script src="js/fancybox/jquery.fancybox.pack.js" type="text/javascript"></script> 
-<script src="js/jquery.nav.js" type="text/javascript"></script> 
-<script src="js/jquery.fittext.js"></script> 
-<script src="js/waypoints.js"></script> 
-<script src="js/custom.js" type="text/javascript"></script> 
-<script src="js/owl-carousel/owl.carousel.js"></script>
+<!-- <script src="js/fancybox/jquery.fancybox.pack.js" type="text/javascript"></script>  -->
+<!-- <script src="js/jquery.nav.js" type="text/javascript"></script>  -->
+<!-- <script src="js/jquery.fittext.js"></script>  -->
+<!-- <script src="js/waypoints.js"></script>  -->
+<!-- <script src="js/custom.js" type="text/javascript"></script>  -->
+<!-- <script src="js/owl-carousel/owl.carousel.js"></script> -->
 </body>
 </html>
